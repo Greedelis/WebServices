ae6393908c2940f9cbfb825c4442a44c7587ece1
Greedelis <andriusbglns0@gmail.com>
2020-04-22 12:09:52 +0300

    ND 2 first commit

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 from flask import Flask, request, jsonify, abort, Response
 import json
-
+import requests 
 app = Flask(__name__)
 
+URL = "http://arnweb:5000/"
 
 parts = [
     {"id" : 0,
      "manufacturer": "AMD",
      "name": "Ryzen 7 3800x",
      "type": "CPU",
-     "price": "331.90"
+     "price": "331.90",
+     "phone_id" : "1"
      },
     {"id": 1,
      "manufacturer": "AMD",
      "name": "Ryzen 5 3600",
      "type": "CPU",
-     "price": "181.00"
+     "price": "181.00",
+     "phone_id" : "2"
      },
     {"id": 2,
      "manufacturer": "ASRock",
      "name": "AB350 PRO4",
      "type": "Motherboard",
-     "price": "74.69"
+     "price": "74.69",
+     "phone_id" : "4"
      }
 ]
 
@@ -35,6 +39,8 @@ new_id = 3
 def home():
     return "<h1>PC parts list</h1><a href='http://localhost:5000/api/parts'>All parts</a>"
 
+
+
 @app.route("/api/parts", methods = ["GET", "POST"])
 def api_parts():
     if request.method == "GET":
@@ -47,14 +53,15 @@ def api_parts():
 
     elif request.method == "POST":
         new_data = request.get_json("force=True")
-        if "name" in new_data and "manufacturer" in new_data and "type" in new_data and "price" in new_data:
+        if "name" in new_data and "manufacturer" in new_data and "type" in new_data and "price" in new_data and "phone_id" in new_data:
             global new_id
             new_part = {
                 "id": new_id,
                 "manufacturer": new_data["manufacturer"],
                 "name": new_data["name"],
                 "type": new_data["type"],
-                "price": new_data["price"]
+                "price": new_data["price"],
+                "phone_id": new_data["phone_id"]
             }
             parts.append(new_part)
             new_id+=1
@@ -69,12 +76,18 @@ def api_parts():
                 error_msg += "type "
             if "price" not in new_data:
                 error_msg += "price "
+            if "phone_id" not in new_data:
+                error_msg += "phone_id "
             error_msg += "have been declared"
 
             return Response(json.dumps({"Failure" : error_msg}),status=400,mimetype="application/json")
     else:
         abort(404)
 
+@app.route("/api/test", methods = ["GET", "DELETE", "PUT"])
+def test():
+    res = (requests.get(URL+"phones"))
+    return jsonify(res.json())
 
 @app.route("/api/parts/<int:part_id>", methods = ["GET", "DELETE", "PUT"])
 def api_part_id(part_id):
@@ -106,13 +119,41 @@ def api_part_id(part_id):
         if "price" in new_data:
             part[0]["price"] = new_data["price"]
             response += "price "
+        if "phone_id" in new_data:
+            part[0]["phone_id"] = new_data["phone_id"]
+            response += "phone_id "
         response += "have been changed"
        
 
-        if "name" not in new_data and "manufacturer" not in new_data and "type" not in new_data and "price" not in new_data:
+        if "name" not in new_data and "manufacturer" not in new_data and "type" not in new_data and "price" not in new_data and "phone_id" not in new_data:
             return Response(json.dumps({"Failed" : "no "}))
 
         return Response(json.dumps(part),status=200, mimetype="application/json")
+
+@app.route("/api/phones", methods = ["GET", "POST"])
+def api_phones():
+    if request.method == "GET":  
+        req = requests.get(URL + "phones/")
+        return jsonify(req.json())
+    elif request.method == "POST":
+        new_data = request.get_json("force=True")
+        resp = requests.post(URL+"phones", json = (new_data), )
+        #return new_data
+        if str(resp.status_code) == "400" or str(resp.status_code) == "404":
+            return Response(json.dumps({"Failure" : resp.text}),status=resp.status_code,mimetype="application/json")
+        else:
+             return Response(json.dumps({"Success" : resp.text}),status=resp.status_code,mimetype="application/json")
+
+@app.route("/api/parts/<int:part_id>/phone", methods = ["GET"])
+def api_phone_info(part_id):
+    part = [part for part in parts if part["id"] == part_id]
+    if len(part) == 0:
+        abort(404)
+    
+    if request.method == "GET":
+        req = requests.get(URL+"phones/"+str(part[0]["phone_id"]))
+        #print (part[0]["phone_id"])
+        return jsonify(req.json())
 
         
 
